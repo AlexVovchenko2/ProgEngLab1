5d8380a4994e92c5d4e8b509a00dac0dd085c1d1
AlexVovchenko2 <alexvov1@outlook.com>
2025-02-22 21:20:07 +0300

    first commit

---
+++ Unit1.h
@@ -0,0 +1,36 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.Grids.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TLabel *LabelRows;
+	TLabel *LabelColumns;
+	TEdit *EditRows;
+	TEdit *EditColumns;
+	TStringGrid *StringGrid1;
+	TButton *Button1;
+	TLabel *size;
+	TLabel *LabelMatrix;
+	TEdit *EditMatrix;
+	TButton *Button2;
+	void __fastcall Button1Click(TObject *Sender);
+	void __fastcall Button2Click(TObject *Sender);
+
+private:	// User declarations
+public:		// User declarations
+    int n, m;
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
